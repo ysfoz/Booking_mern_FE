f95120e18883df778ed27880ab6c5352e5522b06
ysfoz <ysfozturk3@gmail.com>
2022-06-13 14:45:26 +0200

    created Pages and Router

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../components/navbar/Navbar"
+import Header from "../../components/header/Header"
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Header/>
+    </div>
+  )
+}
+
+export default Home
