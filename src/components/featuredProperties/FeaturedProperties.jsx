1733e0ae02646e0e32d8a1afb49dfaf3fa3bd13f
ysfoz <ysfozturk3@gmail.com>
2022-06-16 16:29:21 +0200

    added feturedproperties component

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+  Container,
+  Wrapper,
+  Image,
+  Title,
+  TitleContainer,
+  City,
+  Price,
+  Rating,
+  RatingsContainer,
+  Rewiew,
+  Note,
+} from "./featuredProperties.style";
+
+const FeaturedProperties = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Image src="https://images.unsplash.com/photo-1554009975-d74653b879f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDh8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"/>
+        <TitleContainer>
+          <Title>The Patio Barcelona</Title>
+          <City>Barcelona</City>
+        </TitleContainer>
+        <Price>Starting from 2.199 zt</Price>
+        <RatingsContainer>
+          <Rating>9.3</Rating>
+          <Note>Wanderful</Note>
+          <Rewiew>345 rewiews</Rewiew>
+        </RatingsContainer>
+      </Wrapper>
+      <Wrapper>
+        <Image src="https://images.unsplash.com/photo-1554009975-d74653b879f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDh8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"/>
+        <TitleContainer>
+          <Title>The Patio Barcelona</Title>
+          <City>Barcelona</City>
+        </TitleContainer>
+        <Price>Starting from 2.199 zt</Price>
+        <RatingsContainer>
+          <Rating>9.3</Rating>
+          <Note>Wanderful</Note>
+          <Rewiew>345 rewiews</Rewiew>
+        </RatingsContainer>
+      </Wrapper>
+      <Wrapper>
+        <Image src="https://images.unsplash.com/photo-1554009975-d74653b879f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDh8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"/>
+        <TitleContainer>
+          <Title>The Patio Barcelona</Title>
+          <City>Barcelona</City>
+        </TitleContainer>
+        <Price>Starting from 2.199 zt</Price>
+        <RatingsContainer>
+          <Rating>9.3</Rating>
+          <Note>Wanderful</Note>
+          <Rewiew>345 rewiews</Rewiew>
+        </RatingsContainer>
+      </Wrapper>
+      <Wrapper>
+        <Image src="https://images.unsplash.com/photo-1554009975-d74653b879f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDh8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"/>
+        <TitleContainer>
+          <Title>The Patio Barcelona</Title>
+          <City>Barcelona</City>
+        </TitleContainer>
+        <Price>Starting from 2.199 zt</Price>
+        <RatingsContainer>
+          <Rating>9.3</Rating>
+          <Note>Wanderful</Note>
+          <Rewiew>345 rewiews</Rewiew>
+        </RatingsContainer>
+      </Wrapper>
+     
+    </Container>
+  );
+};
+
+export default FeaturedProperties;
---
+++ src/components/featuredProperties/featuredProperties.style.js
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+display: flex;
+width: 100%;
+max-width: 1024px;
+justify-content: space-between;
+gap: 20px;
+margin-bottom: 250px;
+
+`;
+
+const Wrapper = styled.div`
+flex: 1;
+`;
+
+const Image = styled.img`
+width: 100%;
+height: 200px;
+`;
+
+const TitleContainer = styled.div`
+display: flex;
+flex-direction: column;
+gap: 3px;
+`;
+
+const Title = styled.span`
+font-weight:400;
+font-size: 14px;
+
+`;
+const City = styled.span`
+font-weight:300;
+font-size: 12px;`;
+
+const Price = styled.span`
+font-weight:600;
+font-size: 14px;
+
+`;
+
+const RatingsContainer = styled.div`
+font-size: 14px;
+display: flex;
+gap: 10px;
+margin-top: 10px;
+
+`;
+
+const Rating = styled.button`
+background-color:#003580;
+padding: 2px 4px;
+border-radius: 5px;
+border-bottom-left-radius: 0px;
+color: white;
+font-weight: 600;
+font-size: 14px;
+border:none;
+cursor: pointer;
+:hover{
+    background-color: crimson;
+};
+:active{
+    background-color: aliceblue;
+}
+
+`;
+
+const Note = styled.span`
+font-weight: 400;
+font-size: 14px;
+`;
+
+const Rewiew = styled.span`
+color: lightgray;
+font-weight: 400;
+font-size: 14px;`;
+
+export {
+  Container,
+  Wrapper,
+  Image,
+  Title,
+  TitleContainer,
+  City,
+  Price,
+  Rating,
+  RatingsContainer,
+  Rewiew,
+  Note,
+};
