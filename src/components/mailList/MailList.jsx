67c62a5be94c1523fcf26a6eb0d5cff5361071af
ysfoz <ysfozturk3@gmail.com>
2022-06-16 16:29:42 +0200

    added mailList component

---
+++ src/components/mailList/MailList.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Title, SubTitel, MailContainer, Mail, Button } from './mailList.style'
+
+const MailList = () => {
+  return (
+    <Container>
+<Title>Save time, save money!</Title>
+<SubTitel>Sign up and we'll send the best deals to you</SubTitel>
+<MailContainer>
+    <Mail placeholder='Your email'/>
+    <Button>
+Subscribe
+    </Button>
+</MailContainer>
+    </Container>
+  )
+}
+
+export default MailList
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
 import PropertyList from "../../components/propertList/PropertyList";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import MailList from "../../components/mailList/MailList";
 import { ListContainer, Title } from "./home.style";
 
 const Home = () => {
@@ -13,6 +15,9 @@ const Home = () => {
         <Featured />
         <Title>Browse by property type</Title>
         <PropertyList/>
+        <Title>Homes guests love</Title>
+        <FeaturedProperties/>
+        <MailList/>
       </ListContainer>
     </div>
   );
