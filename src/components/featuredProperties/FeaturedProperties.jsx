7cb44a13c678739bc08c72b99876fc4a1d433cd3
ysfoz <ysfozturk3@gmail.com>
2022-06-21 15:53:48 +0200

    added data to featuredproperties

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import useFetch from "../../hooks/useFetch";
+import { LoadingSpinner } from "../spinner/spinner.style";
 import {
   Container,
   Wrapper,
@@ -14,61 +16,40 @@ import {
 } from "./featuredProperties.style";
 
 const FeaturedProperties = () => {
+  const { data, loading, error } = useFetch(
+    "/hotel?featured=true&limit=4&min=40&max=600"
+  );
+  console.log(
+    "🚀 ~ file: FeaturedProperties.jsx ~ line 20 ~ FeaturedProperties ~ data",
+    data
+  );
+
   return (
-    <Container>
-      <Wrapper>
-        <Image src="https://images.unsplash.com/photo-1554009975-d74653b879f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDh8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" />
-        <TitleContainer>
-          <Title>The Patio Barcelona</Title>
-          <City>Barcelona</City>
-        </TitleContainer>
-        <Price>Starting from 2.199 zt</Price>
-        <RatingsContainer>
-          <Rating>9.3</Rating>
-          <Note>Wanderful</Note>
-          <Rewiew>345 rewiews</Rewiew>
-        </RatingsContainer>
-      </Wrapper>
-      <Wrapper>
-        <Image src="https://images.unsplash.com/photo-1554009975-d74653b879f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDh8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" />
-        <TitleContainer>
-          <Title>The Patio Barcelona</Title>
-          <City>Barcelona</City>
-        </TitleContainer>
-        <Price>Starting from 2.199 zt</Price>
-        <RatingsContainer>
-          <Rating>9.3</Rating>
-          <Note>Wanderful</Note>
-          <Rewiew>345 rewiews</Rewiew>
-        </RatingsContainer>
-      </Wrapper>
-      <Wrapper>
-        <Image src="https://images.unsplash.com/photo-1554009975-d74653b879f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDh8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" />
-        <TitleContainer>
-          <Title>The Patio Barcelona</Title>
-          <City>Barcelona</City>
-        </TitleContainer>
-        <Price>Starting from 2.199 zt</Price>
-        <RatingsContainer>
-          <Rating>9.3</Rating>
-          <Note>Wanderful</Note>
-          <Rewiew>345 rewiews</Rewiew>
-        </RatingsContainer>
-      </Wrapper>
-      <Wrapper>
-        <Image src="https://images.unsplash.com/photo-1554009975-d74653b879f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDh8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" />
-        <TitleContainer>
-          <Title>The Patio Barcelona</Title>
-          <City>Barcelona</City>
-        </TitleContainer>
-        <Price>Starting from 2.199 zt</Price>
-        <RatingsContainer>
-          <Rating>9.3</Rating>
-          <Note>Wanderful</Note>
-          <Rewiew>345 rewiews</Rewiew>
-        </RatingsContainer>
-      </Wrapper>
-    </Container>
+    <>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <Container>
+          {data?.map((hotel, index) => {
+            return (
+              <Wrapper key={index}>
+                <Image src={hotel?.photos[0] || "https://images.unsplash.com/photo-1554009975-d74653b879f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDh8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"} />
+                <TitleContainer>
+                  <Title>{hotel?.name}</Title>
+                  <City>{hotel?.city}</City>
+                </TitleContainer>
+                <Price>Starting from {hotel?.cheapestPrice} zt</Price>
+                <RatingsContainer>
+                  {hotel?.rating && <Rating>{hotel.rating}</Rating>}
+                  <Note>Wanderful</Note>
+                  <Rewiew>345 rewiews</Rewiew>
+                </RatingsContainer>
+              </Wrapper>
+            );
+          })}
+        </Container>
+      )}
+    </>
   );
 };
 
